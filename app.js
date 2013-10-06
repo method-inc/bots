7499cd5c8d10c0cd11f84b0eec6d306fe92bdd4d
Eric LaForce <elaforc@gmail.com>
2013-10-05 22:36:01 -0400

    Printing method

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 var fs = require('fs');
+var _ = require('underscore');
+var util = require('util');
 var game = JSON.parse(fs.readFileSync('initial.json').toString());
 
+function debug_print(o) {
+  util.debug(util.inspect(o, false, 2, true));
+}
+
 function print_map(map) {
-  console.log(map);
+  debug_print(map);
+  util.print('\n\nMap:\n');
+  util.print('[ ');
+
+  for (var i = 0; i < map.rows; i++) {
+    for (j = 0; j < map.cols; j++) {
+      util.print(map.data.charAt(j + i*map.cols) + ' ');
+    }
+
+    if (i < map.rows - 1)
+      util.print('\n  ');
+  }
+
+  util.print(' ]');
 }
 
 print_map(game.map);
