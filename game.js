e9eb854655dc1e43789ae724edc390a41c1f7c74
mobyvb <mobyvb@gmail.com>
2013-11-19 16:45:57 -0500

    basic testing for game init

--- game.js
+++ game.js
@@ -6,34 +6,103 @@ var gridIds = {
   food:'*',
   empty:'.'
 };
+var distance = {
+  move:1,
+  raze:1,
+  attack:1,
+  food:0,
+  spawn:1
+};
 
-exports.create = function() {
-  var gameState = {};
-  gameState.rows = 20;
-  gameState.cols = 20;
-  gameState.grid = Array(gameState.rows*gameState.cols+1).join(gridIds.empty); //create empty grid
+exports.create = function(rows, cols) {
+  var gameState = {
+    rows:rows,
+    cols:cols
+  };
 
-  setCoord(gameState, 1, 1, gridIds.p1Spawn);
-  setCoord(gameState, gameState.cols-2, gameState.rows-2, gridIds.p2Spawn);
+  gameState.p1 = {
+    food:1,
+    spawn:coordToIndex(gameState, {x:1,y:1})
+  };
+  gameState.p2 = {
+    food:1,
+    spawn:coordToIndex(gameState, {x:cols-2,y:rows-2})
+  };
+  gameState.grid = makeEmptyGrid(gameState.rows, gameState.cols);
 
   return gameState;
 };
 
-function getCoord(state, x, y) {
-  var index = state.rows * y + x;
-  return state.grid[index];
+exports.doTurn = function(state, p1Moves, p2Moves) {
+  // move
+  var re = new RegExp('['+gridIds.player1+gridIds.player2+']','g');
+  state.newGrid = state.grid.replace(re, gridIds.empty);
+  p1Moves.forEach(function(move) {
+    if(validMove(state, move, gridIds.player1)) {
+      state = movePlayer(state, move);
+    }
+  });
+  p2Moves.forEach(function(move) {
+    if(validMove(state, move, gridIds.player2)) {
+      state = movePlayer(state, move);
+    }
+  });
+
+  // gather
+
+  // fight
+
+  // raze
+
+  // spawn
+
+  // determine whether to continue/end game
+};
+
+// movement functions
+function validMove(state, move, playerSymbol) {
+  if(move.from<numCoords && move.from>=0
+      && move.to<numCoords && move.to>=0
+      && state.grid[move.from]===playerSymbol
+      && state.grid[move.to]!==gridIds.p1Spawn
+      && state.grid[move.to]!==gridIds.p2Spawn) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
-function setCoord(state, x, y, val) {
-  var index = state.rows * y + x;
-  state.grid = state.grid.substr(0,index) + val + state.grid.substr(index+1);
+function movePlayer(state, move) {
+
 }
 
+
+// grid functions
+function getCoord(state, coord) {
+  var index = coordToIndex(state, coord);
+  return state.grid[index];
+}
+function setCoord(state, coord, val) {
+  var index = coordToIndex(state, coord);
+  state.grid = state.grid.substr(0,index) + val + state.grid.substr(index+1);
+}
 function showGrid(state) {
   for(var y=0; y<state.rows; y++) {
     var row = '';
     for(var x=0; x<state.cols; x++) {
-      row += state.grid[state.rows * y + x];
+      row += state.grid[coordToIndex(state, {x:x,y:y})];
     }
     console.log(row+'\n');
   }
+}
+function indexToCoord(state, index) {
+  var x = index%state.grid.cols;
+  var y = ~~(index/state.grid.cols);
+  return {x:x, y:y};
+}
+function coordToIndex(state, coord) {
+  return state.cols * coord.y + coord.x;
+}
+function makeEmptyGrid(rows, cols) {
+  return Array(rows*cols+1).join(gridIds.empty);
 }
--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,31 @@
-var assert = require("assert")
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-})
+var _ = require('underscore');
+var assert = require('assert');
+var game = require('../game.js');
+
+describe('Game', function(){
+  describe('#create()', function(){
+    it('should properly initialize a game state', function(){
+      var createdState = game.create(5,10);
+      var testState = {
+        rows:5,
+        cols:10,
+        p1:{food:1, spawn:11},
+        p2:{food:1, spawn:38},
+        grid:'..................................................'
+      };
+
+      assert.equal(JSON.stringify(createdState), JSON.stringify(testState));
+
+      createdState = game.create(6,4);
+      testState = {
+        rows:6,
+        cols:4,
+        p1:{food:1, spawn:5},
+        p2:{food:1, spawn:18},
+        grid:'........................'
+      };
+
+      assert.equal(JSON.stringify(createdState), JSON.stringify(testState));
+    });
+  });
+});
