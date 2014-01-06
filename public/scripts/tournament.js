c9e338ac625097f7afb2e553a391281e9f16c49d
mobyvb <mobyvb@gmail.com>
2014-01-06 14:21:43 -0500

    use jquery.gracket.js to display tournament bracket

---
+++ public/scripts/tournament.js
@@ -0,0 +1,35 @@
+gracketData = [];
+empty = 0;
+roundList.forEach(function(round) {
+  roundArray = [];
+  round.forEach(function(game) {
+    var p1 = '';
+    var p2 = '';
+    var p1Id = '';
+    var p2Id = '';
+    if(game.p1) {
+      p1 = game.p1;
+      p1Id = game.p1;
+    }
+    else {
+      p1Id = 'empty' + empty;
+      empty++;
+    }
+    if(game.p2) {
+      p2 = game.p2;
+      p2Id = game.p2;
+    }
+    else {
+      p2Id = 'empty' + empty;
+      empty++;
+    }
+
+    roundArray.push([{name:p1, id:p1Id}, {name:p2, id:p2Id}]);
+  });
+  gracketData.push(roundArray);
+});
+gracketData.push([[ {name: '', id: 'empty'+empty}]]);
+
+$(document).ready(function() {
+  $('.bracket').gracket({src:gracketData});
+});
