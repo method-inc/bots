a69c97164280bdb184f980051f80742af2ba69b2
Patrick Faulkner <ptfaulkner@gmail.com>
2016-09-20 22:14:15 -0400

    update view engine to pug

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(session({
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use('/', routes);
 app.use('/bot', bots);
