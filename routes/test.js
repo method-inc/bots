5c0708610a14124edbd9f91cc350f19241cdc8ad
Patrick Faulkner <ptfaulkner@gmail.com>
2016-09-29 12:08:10 -0400

    fix test route

--- routes/test.js
+++ routes/test.js
@@ -6,20 +6,16 @@ router.get('/', function(req, res) {
   if(!process.env.HEROKU) {
     var sessionUser = req.session.auth.google.user;
 
-    User.findAll()
-      .then(function(users, err) {
+    User.findOne({ where: { googleId: sessionUser.id } })
+      .then(function(user, err) {
         var toSend = [];
         toSend.push({name:'nodebot'});
-
-        users.forEach(function(user) {
-          toSend.push({name:user.email});
-          console.log('sending bots '+ toSend);
-        });
-
+        toSend.push({name:user.email});
+        console.log('sending bots '+ toSend);
         res.render('test', {
           bots: toSend
         });
-      });
+    });
   }
   else {
     res.redirect('/');
