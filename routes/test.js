b0b22a35dce47a8e2e4ea71875c8ed4a056e1695
Patrick Faulkner <ptfaulkner@gmail.com>
2016-10-13 15:54:09 -0400

    open up test route

--- routes/test.js
+++ routes/test.js
@@ -6,16 +6,20 @@ router.get('/', function(req, res) {
   if(!process.env.HEROKU) {
     var sessionUser = req.session.auth.google.user;
 
-    User.findOne({ where: { googleId: sessionUser.id } })
-      .then(function(user, err) {
+    User.findAll()
+      .then(function(users, err) {
         var toSend = [];
         toSend.push({name:'nodebot'});
-        toSend.push({name:user.email});
-        console.log('sending bots '+ toSend);
+
+        users.forEach(function(user) {
+          toSend.push({name:user.email});
+          console.log('sending bots '+ toSend);
+        });
+
         res.render('test', {
           bots: toSend
         });
-    });
+      });
   }
   else {
     res.redirect('/');
